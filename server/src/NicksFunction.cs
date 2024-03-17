a880f1e418e6882d751f462a273f24b2cccda6a5
Nick Sullivan <nick.dave.sullivan@gmail.com>
2024-03-17 10:35:12 +1030

    fix build error

--- server/src/NicksFunction.cs
+++ server/src/NicksFunction.cs
@@ -20,11 +20,5 @@ namespace Company.Function
             _logger.LogInformation("C# HTTP trigger function processed a request.");
             return new OkObjectResult("Welcome to Azure Functions!");
         }
-
-        [Function("Another")]
-        public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
-        {
-            return new OkObjectResult("This is a second response!");
-        }
     }
 }
