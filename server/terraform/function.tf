49db4d390a03e020b57b0f4db9bd145d6bd24d72
Nick Sullivan <nick.dave.sullivan@gmail.com>
2024-03-17 12:21:02 +1030

    trying again

--- server/terraform/function.tf
+++ server/terraform/function.tf
@@ -6,23 +6,13 @@ resource "azurerm_windows_function_app" "example" {
   service_plan_id            = azurerm_service_plan.example.id
   storage_account_name       = azurerm_storage_account.example.name
   storage_account_access_key = azurerm_storage_account.example.primary_access_key
-  # zip_deploy_file = data.archive_file.zip.output_path
   site_config {}
-  # site_config {
-  #   application_stack {
-  #     dotnet_version="8.0"
-  #     use_dotnet_isolated_runtime=true
-  #   }
-  # }
-
   app_settings = {
     FUNCTIONS_WORKER_RUNTIME       = "dotnet-isolated"
     APPINSIGHTS_INSTRUMENTATIONKEY = azurerm_application_insights.example.instrumentation_key
+    # This is required for https://github.com/Azure/webapps-deploy
     WEBSITE_WEBDEPLOY_USE_SCM      = true
-    # WEBSITE_RUN_FROM_PACKAGE       = 1
-    # SCM_DO_BUILD_DURING_DEPLOYMENT = true  # true if zip_deploy_file is specified
   }
-
   lifecycle {
     ignore_changes = [
       # Don't clear deployments
