4a1b543ea54ec9d828de53e6a7ae6e666cbc594c
Nick Sullivan <nick.dave.sullivan@gmail.com>
2024-03-17 18:22:48 +1030

    adding fake test to see if it makes pipeline fail

--- server/test/WhenCallingNicksFunction.cs
+++ server/test/WhenCallingNicksFunction.cs
@@ -19,12 +19,10 @@ public class WhenCallingNicksFunction
     public async Task SetUp()
     {
         _playwright = await Playwright.CreateAsync();
-
         var request = await _playwright.APIRequest.NewContextAsync(new() {
             BaseURL = "https://azureexperiment-dev.azurewebsites.net",
             ExtraHTTPHeaders = new Dictionary<string, string>(),
         });
-
         _response = await request.GetAsync("/api/NicksFunction");
     }
 
@@ -38,7 +36,14 @@ public class WhenCallingNicksFunction
     public async Task ItShouldReturnExpectedString()
     {
         var text = await _response.TextAsync();
-        Assert.AreEqual("Welcome to Azure Functions!", text);
+        Assert.That(text, Is.EqualTo("Welcome to Azure Functions!"))
+    }
+
+    [Test]
+    public async Task DeliberateFailure()
+    {
+        var text = await _response.TextAsync();
+        Assert.That(text, Is.EqualTo("Something else!"))
     }
 
 }
